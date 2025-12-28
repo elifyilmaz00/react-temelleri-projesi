3b490a626c77e55761aca8f25577ed3c06d83f0e
elifyilmaz00 <elifyilmaz1906@gmail.com>
2025-12-28 17:19:00 +0300

    feat(component): implement list rendering by mapping data to ProfilKarti

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,57 @@
-// Stil dosyasını import etmeye devam edebiliriz
+//CSS dosyasını import ettik
 import './App.css';
+//profilkarti comp. import ettik
+import ProfilKarti from './ProfilKarti.jsx';
 
-// 'App' adında, büyük harfle başlayan bir fonksiyon
-function App() {
-  const ad = "Elif";
-  const projeAdi = "React Temelleri";
-  
-  // Bu fonksiyon bir JSX ifadesi return ediyor
-  return (
-    // Tek bir kök element olan div ile sarmalıyoruz
-    <div>
-      <h1>{projeAdi} Projesine Hoş Geldin!</h1>
-      <p>Bu ilk React component'ini düzenleyen: {ad}</p>
-      <p>Şu anki saat: {new Date().toLocaleTimeString()}</p>
+//kullanacağımız verileri bir dizi içinde tutalım
+
+const kullanicilar = [
+  {
+    ad:"Leonardo DiCaprio",
+    meslek: "Aktör",
+    resim:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Leonardo_DiCaprio_66ème_Festival_de_Venise_%28Mostra%29.jpg/440px-Leonardo_DiCaprio_66ème_Festival_de_Venise_%28Mostra%29.jpg"
+
+  },
+
+  {
+    ad: "Elif Öğrenci",
+    meslek: "Yazılım Geliştirici",
+    resim: 'https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=500'
+  },
+
+  {
+    ad: "John Doe",
+    meslek: "Grafik Tasarımcı",
+    resim: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=500'
+  }
+];
+
+function App(){
+  return(
+    //<>fragment kullanımı greksiz div olusturmamak için
+    <>
+    <h1>Ekip Üyeleri</h1>
+    <div style={{display: "flex", justifyContent: "center", flexWrap:"wrap"}}>
+      {
+        /*
+          JavaScript'in .map() metodunu kullanarak 'kullanicilar' dizisindeki her bir
+          eleman için bir 'ProfilKarti' component'i oluşturuyoruz.*/
+      }
+      {
+        kullanicilar.map((kullanici,index)=>(
+          <ProfilKarti
+            key={index} //reactin listeleri verimli yönetmesi için gerekli özel prop
+            ad={kullanici.ad}
+            meslek={kullanici.meslek}
+            resim= {kullanici.resim}
+          />
+        ))}
+      
     </div>
+    
+    </>
   );
 }
 
-// Bu satır, bu component'i başka dosyaların (main.jsx gibi) import edebilmesi için dışa aktarır.
 export default App;
+
